61a4ea14d5cbd01a7084f1374446099b68ba6cac
Teodor <teodortenchev@gmail.com>
2018-11-17 14:02:07 +0400

    Adding solution to the 2nd task
    All tests passed.

---
+++ C# OOP Basics/Inheritance/P2_BookShop/Book.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+
+namespace P2_BookShop
+{
+    public class Book
+    {
+        private string author;
+        private string title;
+        private decimal price;
+
+        public Book(string author, string title, decimal price)
+        {
+            Author = author;
+            Title = title;
+            Price = price;
+        }
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            protected set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Title not valid!");
+                }
+
+                title = value;
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+            protected set
+            {
+                string[] names = value.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+                if (names.Length > 1)
+                {
+                    if (char.IsDigit(names[1][0]))
+                    {
+                        throw new ArgumentException("Author not valid!");
+                    }
+                }
+                this.author = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return price;
+            }
+            protected set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Price not valid!");
+                }
+                price = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            string lineBreak = Environment.NewLine;
+
+            return "Type: Book" + lineBreak + $"Title: {Title}" + lineBreak +
+                $"Author: {Author}" + lineBreak + $"Price: {Price:F2}";
+        }
+    }
+}
---
+++ C# OOP Basics/Inheritance/P2_BookShop/GoldenEditionBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P2_BookShop
+{
+    public class GoldenEditionBook : Book
+    {
+        public GoldenEditionBook(string author, string title, decimal price) : base(author, title, price)
+        {
+            Price += Decimal.Multiply(Price, (decimal)0.3);
+        }
+
+        public override string ToString()
+        {
+            string lineBreak = Environment.NewLine;
+
+            return "Type: GoldenEditionBook" + lineBreak + $"Title: {Title}" + lineBreak +
+                $"Author: {Author}" + lineBreak + $"Price: {Price:F2}";
+        }
+    }
+}
