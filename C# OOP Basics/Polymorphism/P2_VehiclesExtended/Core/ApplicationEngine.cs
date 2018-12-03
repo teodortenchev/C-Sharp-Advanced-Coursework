0f4c8b1506646c45c57b8d6c4e46fc4afb9fc850
Teodor <teodortenchev@gmail.com>
2018-12-03 16:17:06 +0400

    Solution to Task 2

---
+++ C# OOP Basics/Polymorphism/P2_VehiclesExtended/Core/ApplicationEngine.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Vehicles.Contracts;
+using Vehicles.Models;
+
+namespace Vehicles.Core
+{
+    public class ApplicationEngine
+    {
+        private IVehicle car;
+        private IVehicle truck;
+        private IVehicle bus;
+
+        public ApplicationEngine()
+        {
+            string[] carInfo = Console.ReadLine().Split();
+            string[] truckInfo = Console.ReadLine().Split();
+            string[] busInfo = Console.ReadLine().Split();
+
+            try
+            {
+                car = new Car(double.Parse(carInfo[1]), double.Parse(carInfo[2]), double.Parse(carInfo[3]));
+                truck = new Truck(double.Parse(truckInfo[1]), double.Parse(truckInfo[2]), double.Parse(truckInfo[3]));
+                bus = new Bus(double.Parse(busInfo[1]), double.Parse(busInfo[2]), double.Parse(busInfo[3]));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void Run()
+        {
+            int commandsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < commandsCount; i++)
+            {
+                string[] commandArgs = Console.ReadLine().Split();
+
+                ExecuteCommand(commandArgs);
+            }
+
+            PrintResults();
+
+
+        }
+
+        private void ExecuteCommand(string[] commandArgs)
+        {
+            if (commandArgs[0] == "Drive")
+            {
+                double distance = double.Parse(commandArgs[2]);
+                if (commandArgs[1] == "Car")
+                {
+                    try
+                    {
+                        car.Drive(distance);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else if (commandArgs[1] == "Truck")
+                {
+                    try
+                    {
+                        truck.Drive(distance);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else if (commandArgs[1] == "Bus")
+                {
+                    try
+                    {
+                        bus.Drive(distance);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                
+            }
+
+            else if (commandArgs[0] == "DriveEmpty")
+            {
+                double distance = double.Parse(commandArgs[2]);
+
+                try
+                {
+                    Bus emptyBus = bus as Bus;
+                    emptyBus?.DriveEmpty(distance);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else if (commandArgs[0] == "Refuel")
+            {
+                double fuelAmount = double.Parse(commandArgs[2]);
+
+                try
+                {
+                    if (commandArgs[1] == "Car")
+                    {
+                        car.Refuel(fuelAmount);
+                    }
+                    else if (commandArgs[1] == "Truck")
+                    {
+                        truck.Refuel(fuelAmount);
+                    }
+                    else if (commandArgs[1] == "Bus")
+                    {
+                        bus.Refuel(fuelAmount);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+            }
+        }
+
+        private void PrintResults()
+        {
+            Console.WriteLine(car);
+            Console.WriteLine(truck);
+            Console.WriteLine(bus);
+        }
+    }
+
+
+
+}
+
---
+++ C# OOP Basics/Polymorphism/P3_WildFarm/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P3_WildFarm
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
