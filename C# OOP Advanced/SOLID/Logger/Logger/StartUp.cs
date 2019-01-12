f73888ed8770edd6a68dada146aede644a6319bc
Teodor <teodortenchev@gmail.com>
2019-01-12 20:48:13 +0400

    added the engine and command interpreter to startup

--- C# OOP Advanced/SOLID/Logger/Logger/StartUp.cs
+++ C# OOP Advanced/SOLID/Logger/Logger/StartUp.cs
@@ -1,30 +1,15 @@
 ﻿namespace Logger
 {
-    using Appenders;
-    using Appenders.Contracts;
-    using Layouts;
-    using Layouts.Contracts;
-    using Logger.Loggers.Enums;
-    using Loggers;
-    using Loggers.Contracts;
+    using Logger.Core;
+    using Logger.Core.Contracts;
 
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            var simpleLayout = new SimpleLayout();
-            var consoleAppender = new ConsoleAppender(simpleLayout);
-            consoleAppender.ReportLevel = ReportLevel.Critical;
-
-            var logger = new Logger(consoleAppender);
-
-            logger.Info("3/31/2015 5:33:07 PM", "Everything seems fine");
-            logger.Warning("3/31/2015 5:33:07 PM", "Warning: ping is too high - disconnect imminent");
-            logger.Error("3/31/2015 5:33:07 PM", "Error parsing request");
-            logger.Critical("3/31/2015 5:33:07 PM", "No connection string found in App.config");
-            logger.Fatal("3/31/2015 5:33:07 PM", "mscorlib.dll does not respond");
-
-
+            ICommandInterpreter commandInterpreter = new CommandInterpreter();
+            IEngine engine = new Engine(commandInterpreter);
+            engine.Run();
         }
     }
 }
