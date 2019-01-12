237135c1674b95e93fce8787ddf3a3ca06ec4330
Teodor <teodortenchev@gmail.com>
2019-01-12 20:46:44 +0400

    Create AppenderFactory.cs

---
+++ C# OOP Advanced/SOLID/Logger/Logger/Appenders/Factory/AppenderFactory.cs
@@ -0,0 +1,27 @@
+﻿namespace Logger.Appenders.Factory
+{
+    using Contracts;
+    using Layouts.Contracts;
+    using Loggers;
+    using System;
+
+    public class AppenderFactory
+    {
+        public IAppender CreateAppender(string type, ILayout layout)
+        {
+            switch (type)
+            {
+                case "ConsoleAppender":
+                    return new ConsoleAppender(layout);
+                    
+                case "FileAppender":
+                    return new FileAppender(layout, new LogFile());
+                   
+
+                default:
+                    throw new ArgumentException("Invalid appender type.");
+                    
+            }
+        }
+    }
+}
