d5fb80022a7bef223f88f96add09ed7431051ec7
Teodor <teodortenchev@gmail.com>
2019-01-12 20:47:31 +0400

    Created Engine and factory

---
+++ C# OOP Advanced/SOLID/Logger/Logger/Core/Engine.cs
@@ -0,0 +1,39 @@
+﻿namespace Logger.Core
+{
+    using Contracts;
+    using System;
+
+    public class Engine : IEngine
+    {
+        private ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpeter)
+        {
+            this.commandInterpreter = commandInterpeter;
+        }
+
+        public void Run()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputArgs = Console.ReadLine().Split();
+                this.commandInterpreter.AddAppender(inputArgs);
+            }
+
+            string input = Console.ReadLine();
+
+            while(input != "END")
+            {
+                string[] inputArgs = input.Split('|');
+                this.commandInterpreter.AddMessage(inputArgs);
+
+
+                input = Console.ReadLine();
+            }
+
+            commandInterpreter.PrintAppenders();
+        }
+    }
+}
---
+++ C# OOP Advanced/SOLID/Logger/Logger/Layouts/Factory/LayoutFactory.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Logger.Layouts.Factory
+{
+    using System;
+
+    using Contracts;
+
+    public class LayoutFactory
+    {
+        public ILayout CreateLayout(string type)
+        {
+            switch (type)
+            {
+                case "SimpleLayout":
+                    return new SimpleLayout();
+
+                case "XmlLayout":
+                    return new XmlLayout();
+
+                default:
+                    throw new ArgumentException("Wrong layout type requested.");
+                   
+            }
+        }
+    }
+}
