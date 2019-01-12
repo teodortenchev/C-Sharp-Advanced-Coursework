0898f5a38d0e0b68cd7c8352023a2be4a895da58
Teodor <teodortenchev@gmail.com>
2019-01-12 20:45:58 +0400

    Created abstract class Appender to group common functionality of all appenders

---
+++ C# OOP Advanced/SOLID/Logger/Logger/Appenders/Appender.cs
@@ -0,0 +1,29 @@
+﻿namespace Logger.Appenders
+{
+    using Contracts;
+    using Layouts.Contracts;
+    using Loggers.Enums;
+
+    public abstract class Appender : IAppender
+    {
+        protected readonly ILayout layout;
+
+        public string LayoutType => layout.GetType().Name;
+        public int MessagesAppended { get; protected set; }
+
+        public ReportLevel ReportLevel { get; set; }
+
+
+        protected Appender(ILayout layout)
+        {
+            this.layout = layout;
+            MessagesAppended = 0;
+        }
+
+        public abstract void Append(string dateTime, ReportLevel reportLevel, string message);
+
+
+        public abstract string Status();
+    
+    }
+}
