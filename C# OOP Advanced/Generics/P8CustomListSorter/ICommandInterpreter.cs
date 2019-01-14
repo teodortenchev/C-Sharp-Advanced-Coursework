2e9a047d5f3372ea554e1a00d0f37cb7876b9894
Teodor <teodortenchev@gmail.com>
2019-01-14 21:18:21 +0400

    P8 Solution

---
+++ C# OOP Advanced/Generics/P8CustomListSorter/ICommandInterpreter.cs
@@ -0,0 +1,15 @@
+﻿namespace P8CustomListSorter
+{
+    public interface ICommandInterpreter<T>
+    {
+        void Add(T element);
+        void Remove(int index);
+        void Contains(T element);
+        void Swap(int index1, int index2);
+        void Greater(T element);
+        void Max();
+        void Min();
+        void Print();
+        void Sort();
+    }
+}
---
+++ C# OOP Advanced/Generics/P8CustomListSorter/StartUp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace P8CustomListSorter
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var commandInterpreter = new CommandInterpreter<string>();
+
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                string[] inputArgs = input.Split();
+                string command = inputArgs[0];
+
+                switch (command)
+                {
+                    case "Add":
+                        commandInterpreter.Add(inputArgs[1]);
+                        break;
+                    case "Remove":
+                        commandInterpreter.Remove(int.Parse(inputArgs[1]));
+                        break;
+                    case "Contains":
+                        commandInterpreter.Contains(inputArgs[1]);
+                        break;
+                    case "Swap":
+                        commandInterpreter.Swap(int.Parse(inputArgs[1]), int.Parse(inputArgs[2]));
+                        break;
+                    case "Greater":
+                        commandInterpreter.Greater(inputArgs[1]);
+                        break;
+                    case "Max":
+                        commandInterpreter.Max();
+                        break;
+                    case "Min":
+                        commandInterpreter.Min();
+                        break;
+                    case "Sort":
+                        commandInterpreter.Sort();
+                        break;
+                    case "Print":
+                        commandInterpreter.Print();
+                        break;
+                    default:
+                        throw new ArgumentException("Invalid command");
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
