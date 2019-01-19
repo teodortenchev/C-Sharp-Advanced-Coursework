7ec7819f17ea24fe86165237d184cad33bab83d9
Teodor <teodortenchev@gmail.com>
2019-01-19 17:46:15 +0400

    Solution to problem 7

---
+++ C# OOP Advanced/IteratorsAndComparators/P07_EqualityLogic/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P07_EqualityLogic
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; }
+        public int Age { get; }
+
+        public int CompareTo(Person other)
+        {
+            int result = this.Name.CompareTo(other.Name);
+
+            if (result == 0)
+            {
+                result = this.Age.CompareTo(other.Age);
+            }
+
+            return result;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Person);
+        }
+
+        public bool Equals(Person otherPerson)
+        {
+            if (this.CompareTo(otherPerson) == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 23 + Name.GetHashCode();
+                hash = hash * 23 + Age.GetHashCode();
+                return hash;
+            }
+        }
+    }
+}
---
+++ C# OOP Advanced/IteratorsAndComparators/P07_EqualityLogic/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P07_EqualityLogic
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            SortedSet<Person> sortedPeople = new SortedSet<Person>();
+            HashSet<Person> hashedPeople = new HashSet<Person>();
+
+            
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                Person person = new Person(name, age);
+
+                sortedPeople.Add(person);
+                hashedPeople.Add(person);
+            }
+
+            Console.WriteLine(sortedPeople.Count);
+            Console.WriteLine(hashedPeople.Count);
+
+        }
+    }
+}
