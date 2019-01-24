a8aa34fdea8f5d20558a7bc08068c42593717647
Teodor <teodortenchev@gmail.com>
2019-01-24 13:36:53 +0400

    Solution to problems 1 and 2

---
+++ C# OOP Advanced/Reflection-Lab/P01_Stealer/Spy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+public class Spy
+{
+    public string StealFieldInfo(string target, params string[] fieldNames)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb.AppendLine($"Class under investigation: {target}");
+
+        Type targetClass = Type.GetType(target);
+
+        FieldInfo[] classFields = targetClass.GetFields(BindingFlags.NonPublic | BindingFlags.Instance
+            | BindingFlags.Public | BindingFlags.Static);
+
+        //creates an instance of the target class object with no parameters in constructor.
+        object instance = Activator.CreateInstance(targetClass, new object[] { });
+
+        foreach (FieldInfo field in classFields.Where(f => fieldNames.Contains(f.Name)))
+        {
+            sb.AppendLine($"{field.Name} = {field.GetValue(instance)}");
+           
+        }
+
+        return sb.ToString().Trim();
+    }
+}
---
+++ C# OOP Advanced/Reflection-Lab/P01_Stealer/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class StartUp
+{
+    static void Main(string[] args)
+    {
+        Spy spy = new Spy();
+
+        string result = spy.StealFieldInfo("Hacker", "username", "password");
+
+        Console.WriteLine(result);
+    }
+}
+
