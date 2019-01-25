2dffa33c83377d00826fb821f728c7ad532bfee0
Teodor <teodortenchev@gmail.com>
2019-01-25 15:43:09 +0400

    P02 Solution

---
+++ C# OOP Advanced/Reflection-Exercises/P02_BlackBoxInteger/BlackBoxIntegerTests.cs
@@ -0,0 +1,46 @@
+﻿namespace P02_BlackBoxInteger
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class BlackBoxIntegerTests
+    {
+        public static void Main()
+        {
+            Type targetClass = typeof(BlackBoxInteger);
+            var instance = Activator.CreateInstance(targetClass, true);
+
+            MethodInfo[] methods = targetClass.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                string[] commandArgs = command.Split('_');
+                string commandName = commandArgs[0];
+                int value = int.Parse(commandArgs[1]);
+
+
+                MethodInfo method = (MethodInfo)methods.FirstOrDefault(m => m.Name == commandName)
+                    .Invoke(instance, new object[] { value });
+
+                FieldInfo field = targetClass.GetField("innerValue", BindingFlags.NonPublic | BindingFlags.Instance);
+
+                var result = field.GetValue(instance);
+
+                Console.WriteLine(result);
+
+                command = Console.ReadLine();
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
