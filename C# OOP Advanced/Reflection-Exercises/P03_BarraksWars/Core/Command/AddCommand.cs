4d3b5cda3b4d86dd1685d31d416ee2ca4c18ade5
Teodor <teodortenchev@gmail.com>
2019-01-25 22:39:44 +0400

    Moved files to Command Folder

---
+++ C# OOP Advanced/Reflection-Exercises/P03_BarraksWars/Core/Command/AddCommand.cs
@@ -0,0 +1,22 @@
+﻿
+namespace P03_BarraksWars.Core.Commmand
+{
+    using P03_BarraksWars.Contracts;
+
+    public class AddCommand : Command
+    {
+        public AddCommand(string[] data, IRepository repository, IUnitFactory unitFactory)
+            : base(data, repository, unitFactory)
+        {
+        }
+
+        public override string Execute()
+        {
+            string unitType = Data[1];
+            IUnit unitToAdd = UnitFactory.CreateUnit(unitType);
+            Repository.AddUnit(unitToAdd);
+            string output = unitType + " added!";
+            return output;
+        }
+    }
+}
---
+++ C# OOP Advanced/Reflection-Exercises/P03_BarraksWars/Core/Command/RetireCommand.cs
@@ -0,0 +1,22 @@
+﻿namespace P03_BarraksWars.Core.Commmand
+{
+    using P03_BarraksWars.Contracts;
+
+    public class RetireCommand : Command
+    {
+        public RetireCommand(string[] data, IRepository repository, IUnitFactory unitFactory)
+            : base(data, repository, unitFactory)
+        {
+        }
+
+        public override string Execute()
+        {
+            string unitType = Data[1];
+            Repository.RemoveUnit(unitType);
+
+            return $"{unitType} retired!";
+        }
+
+
+    }
+}
