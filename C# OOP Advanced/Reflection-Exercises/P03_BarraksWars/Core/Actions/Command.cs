9fbe29d23ac755ebef05dd615aabf8b3c77a52fa
Teodor <teodortenchev@gmail.com>
2019-01-25 19:46:32 +0400

    Create Command.cs

---
+++ C# OOP Advanced/Reflection-Exercises/P03_BarraksWars/Core/Actions/Command.cs
@@ -0,0 +1,37 @@
+﻿namespace P03_BarraksWars.Core.Actions
+{
+    using Contracts;
+    public abstract class Command : IExecutable
+    {
+        private string[] data;
+        private IRepository repository;
+        private IUnitFactory unitFactory;
+
+        protected Command(string[] data, IRepository repository, IUnitFactory unitFactory)
+        {
+            Data = data;
+            Repository = repository;
+            UnitFactory = unitFactory;
+        }
+
+        protected string[] Data
+        {
+            get { return data; }
+            private set { data = value; }
+        }
+        protected IRepository Repository
+        {
+            get { return repository; }
+            private set { repository = value; }
+        }
+
+        protected IUnitFactory UnitFactory
+        {
+            get { return unitFactory; }
+            private set { unitFactory = value; }
+        }
+
+        public abstract string Execute();
+
+    }
+}
