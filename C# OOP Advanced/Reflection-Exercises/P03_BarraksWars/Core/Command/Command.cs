2f3e88893b022cf83a90eaa8d1049c9bc866687c
Teodor <teodortenchev@gmail.com>
2019-01-26 11:26:25 +0400

    Leaving only what's common between all commands
    Preparing for DI.

--- C# OOP Advanced/Reflection-Exercises/P03_BarraksWars/Core/Command/Command.cs
+++ C# OOP Advanced/Reflection-Exercises/P03_BarraksWars/Core/Command/Command.cs
@@ -5,31 +5,16 @@
     public abstract class Command : IExecutable
     {
         private string[] data;
-        private IRepository repository;
-        private IUnitFactory unitFactory;
 
-        protected Command(string[] data, IRepository repository, IUnitFactory unitFactory)
+        protected Command(string[] data)
         {
             Data = data;
-            Repository = repository;
-            UnitFactory = unitFactory;
         }
 
         protected string[] Data
         {
             get { return data; }
             private set { data = value; }
-        }
-        protected IRepository Repository
-        {
-            get { return repository; }
-            private set { repository = value; }
-        }
-
-        protected IUnitFactory UnitFactory
-        {
-            get { return unitFactory; }
-            private set { unitFactory = value; }
         }
 
         public abstract string Execute();
