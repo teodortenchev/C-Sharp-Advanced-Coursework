96181572fb8381974d193af7f20dabcef835e890
Teodor <teodortenchev@gmail.com>
2019-01-27 13:02:26 +0400

    Create Emerald.cs

---
+++ C# OOP Advanced/Reflection-Exercises/P04_InfernoInfinity/Models/Gems/Emerald.cs
@@ -0,0 +1,14 @@
+﻿namespace P04_InfernoInfinity.Models.Gems
+{
+    public class Emerald : Gem
+    {
+        private const int strength = 1;
+        private const int agility = 4;
+        private const int vitality = 9;
+
+        public Emerald(string gemType)
+            : base(gemType, strength, agility, vitality)
+        {
+        }
+    }
+}
