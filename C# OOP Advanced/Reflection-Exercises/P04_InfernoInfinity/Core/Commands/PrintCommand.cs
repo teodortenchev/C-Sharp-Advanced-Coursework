8c7e2d7fab99be6f3150c38526f37bca797f307d
Teodor <teodortenchev@gmail.com>
2019-01-28 21:40:58 +0400

    Create PrintCommand.cs

---
+++ C# OOP Advanced/Reflection-Exercises/P04_InfernoInfinity/Core/Commands/PrintCommand.cs
@@ -0,0 +1,21 @@
+﻿namespace P04_InfernoInfinity.Core.Commands
+{
+    using Contracts;
+
+    public class PrintCommand : Command
+    {
+        [Inject]
+        private IInventory inventory;
+
+        public PrintCommand(string[] data, IInventory inventory) : base(data)
+        {
+            this.inventory = inventory;
+        }
+
+        public override void Execute()
+        {
+            string weaponName = Data[0];
+            inventory.PrintWeapon("a");
+        }
+    }
+}
