8a7a3500f1e1feda0e8e0fac8c34c7879a88e6e4
Teodor <teodortenchev@gmail.com>
2019-01-28 21:50:15 +0400

    Implemented rarity factory

--- C# OOP Advanced/Reflection-Exercises/P04_InfernoInfinity/Contracts/IRarityFactory.cs
+++ C# OOP Advanced/Reflection-Exercises/P04_InfernoInfinity/Contracts/IRarityFactory.cs
@@ -2,6 +2,6 @@
 {
     public interface IRarityFactory
     {
-        IRarity CreateRarity(string rarity);
+        IRarity CreateRarity(string rarityType);
     }
 }
---
+++ C# OOP Advanced/Reflection-Exercises/P04_InfernoInfinity/Core/Factories/RarityFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace P04_InfernoInfinity.Core.Factories
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using Contracts;
+
+    public class RarityFactory : IRarityFactory
+    {
+        public IRarity CreateRarity(string rarityType)
+        {
+            Type rarity = Assembly.GetExecutingAssembly().GetTypes().FirstOrDefault(t => t.Name == rarityType);
+
+            IRarity rarityInstance = (IRarity)Activator.CreateInstance(rarity);
+
+            return rarityInstance;
+        }
+    }
+}
