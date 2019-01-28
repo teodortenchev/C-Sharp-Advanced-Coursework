1ac69d3e928d6ee8d71432c057cca45e63f2e02f
Teodor <teodortenchev@gmail.com>
2019-01-28 21:40:48 +0400

    Create AddCommand.cs

---
+++ C# OOP Advanced/Reflection-Exercises/P04_InfernoInfinity/Core/Commands/AddCommand.cs
@@ -0,0 +1,30 @@
+﻿namespace P04_InfernoInfinity.Core.Commands
+{
+    using Contracts;
+
+    public class AddCommand : Command
+    {
+        [Inject]
+        private IInventory inventory;
+        [Inject]
+        private IGemFactory gemFactory;
+
+        public AddCommand(string[] data, IInventory inventory) : base(data)
+        {
+            this.inventory = inventory;
+        }
+
+        public override void Execute()
+        {
+            string weaponName = Data[0];
+            int gemSlot = int.Parse(Data[1]);
+            string[] gemInfo = Data[2].Split();
+            string gemQuality = gemInfo[0];
+            string gemType = gemInfo[1];
+
+            IGem gem = gemFactory.CreateGem(gemType, gemQuality);
+
+            inventory.AddGemToSocket(weaponName, gemSlot, gem);
+        }
+    }
+}
