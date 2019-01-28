776c2fb27281ff36f7e432723536f71d7b16bee5
Teodor <teodortenchev@gmail.com>
2019-01-28 22:09:10 +0400

    Create WeaponFactory.cs

---
+++ C# OOP Advanced/Reflection-Exercises/P04_InfernoInfinity/Core/Factories/WeaponFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace P04_InfernoInfinity.Core.Factories
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using Contracts;
+
+    public class WeaponFactory : IWeaponFactory
+    {
+        public IWeapon CreateWeapon(IRarity rarity, string name, string type)
+        {
+            Type weaponType = Assembly.GetExecutingAssembly().GetTypes().FirstOrDefault(t => t.Name == type);
+
+            IWeapon weapon = (IWeapon)Activator.CreateInstance(weaponType, new object[] { rarity, name });
+
+            return weapon;
+        }
+    }
+}
