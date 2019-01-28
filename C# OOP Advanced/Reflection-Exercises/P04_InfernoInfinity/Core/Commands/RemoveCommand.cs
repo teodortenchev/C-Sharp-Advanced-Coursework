9a858cd71180d099623104c4c0d1b2c1921651a0
Teodor <teodortenchev@gmail.com>
2019-01-28 21:41:03 +0400

    Create RemoveCommand.cs

---
+++ C# OOP Advanced/Reflection-Exercises/P04_InfernoInfinity/Core/Commands/RemoveCommand.cs
@@ -0,0 +1,27 @@
+﻿namespace P04_InfernoInfinity.Core.Commands
+{
+    using Contracts;
+    public class RemoveCommand : Command
+    {
+        [Inject]
+        private IInventory inventory;
+
+        public RemoveCommand(string[] data, IInventory inventory) : base(data)
+        {
+            this.inventory = inventory;
+        }
+
+        public override void Execute()
+        {
+            string weaponName = Data[0];
+            int gemSlot = int.Parse(Data[1]);
+            IWeapon weapon = inventory.Weapons[weaponName];
+            IGem gem = weapon.Sockets[gemSlot];
+
+            if (gem != null)
+            {
+                weapon.RemoveGem(gemSlot, gem);
+            }
+        }
+    }
+}
