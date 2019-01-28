8a3c09cab467df286c684963aef6e11757ec8069
Teodor <teodortenchev@gmail.com>
2019-01-28 21:40:53 +0400

    Create CreateCommand.cs

---
+++ C# OOP Advanced/Reflection-Exercises/P04_InfernoInfinity/Core/Commands/CreateCommand.cs
@@ -0,0 +1,40 @@
+﻿namespace P04_InfernoInfinity.Core.Commands
+{
+    using Contracts;
+    public class CreateCommand : Command
+    {
+        [Inject]
+        private IInventory inventory;
+        [Inject]
+        private IWeaponFactory weaponFactory;
+        [Inject]
+        private IRarityFactory rarityFactory;
+
+
+        public CreateCommand(string[] data, IInventory inventory, 
+            IWeaponFactory weaponFactory, IRarityFactory rarityFactory) : base(data)
+        {
+            this.inventory = inventory;
+            this.weaponFactory = weaponFactory;
+            this.rarityFactory = rarityFactory;
+            
+        }
+
+        public override void Execute()
+        {
+            string[] part1 = Data[0].Split();
+
+            string rarityType = part1[0];
+            string weaponType = part1[1];
+
+            string weaponName = Data[1];
+
+            IRarity rarity = rarityFactory.CreateRarity(rarityType);
+            IWeapon weapon = weaponFactory.CreateWeapon(rarity, weaponName, weaponType);
+
+            inventory.AddWeapon(weapon);
+
+
+        }
+    }
+}
